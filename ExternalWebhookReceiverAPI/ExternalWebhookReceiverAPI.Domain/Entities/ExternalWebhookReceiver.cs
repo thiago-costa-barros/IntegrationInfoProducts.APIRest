67bc402fe6518b21cd22217df64fbd20b5a197df
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-01 06:48:57 -0300

    remove: arquivos nao utilizados

---
+++ ExternalWebhookReceiverAPI/ExternalWebhookReceiverAPI.Domain/Entities/ExternalWebhookReceiver.cs
@@ -0,0 +1,18 @@
+﻿using CommonSolution.Entities;
+using CommonSolution.Interfaces;
+
+namespace ExternalWebhookReceiverAPI.Domain.Entities
+{
+    public class ExternalWebhookReceiver : AuditableEntity
+    {
+        public int ExternalWebhookReceiverId { get; set; }
+
+        public int SourceType { get; set; }
+        public int Status { get; set; }
+        public int CompanyId { get; set; }
+        public string ExternalIdentifier { get; set; }
+        public string Payload { get; set; }
+        [ForeignKey(nameof(CompanyId))]
+        public Company Company { get; set; }
+    }
+}
