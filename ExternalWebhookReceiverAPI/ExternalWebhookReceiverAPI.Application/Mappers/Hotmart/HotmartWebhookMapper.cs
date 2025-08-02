16e57460d88e0c9c812c794e06a88d833f18c618
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-08-02 17:52:01 -0300

    feat: add HotmartWebhookMapper

---
+++ ExternalWebhookReceiverAPI/ExternalWebhookReceiverAPI.Application/Mappers/Hotmart/HotmartWebhookMapper.cs
@@ -0,0 +1,27 @@
+﻿using CommonSolution.Entities.CoreSchema;
+using ExternalWebhookReceiverAPI.Application.DTOs.Hotmart;
+using ExternalWebhookReceiverAPI.Domain.Entities;
+using ExternalWebhookReceiverAPI.Domain.Entities.Enums;
+using System.Text.Json;
+
+namespace ExternalWebhookReceiverAPI.Application.Mappings.Hotmart
+{
+    public class HotmartWebhookMapper
+    {
+        public static ExternalWebhookReceiver ToExternalWebhookReceiver(
+            HotmartWebhookDTO dto,
+            Company company,
+            ExternalWebhookReceiverSourceType sourceType)
+        {
+            return new ExternalWebhookReceiver
+            {
+                SourceType = sourceType,
+                Status = ExternalWebhookReceiverStatus.Created,
+                CompanyId = company.CompanyId,
+                ExternalIdentifier = dto.Id,
+                Payload = JsonSerializer.SerializeToElement(dto),
+                Company = company,
+            };
+        }
+    }
+}
--- ExternalWebhookReceiverAPI/ExternalWebhookReceiverAPI.Application/Services/Hotmart/HotmartPurchaseWebhookService.cs
+++ ExternalWebhookReceiverAPI/ExternalWebhookReceiverAPI.Application/Services/Hotmart/HotmartPurchaseWebhookService.cs
@@ -4,6 +4,9 @@ using ExternalWebhookReceiverAPI.Application.DTOs.Common;
 using ExternalWebhookReceiverAPI.Application.DTOs.Hotmart;
 using ExternalWebhookReceiverAPI.Application.Interfaces.Common;
 using ExternalWebhookReceiverAPI.Application.Interfaces.Hotmart;
+using ExternalWebhookReceiverAPI.Application.Mappings.Hotmart;
+using ExternalWebhookReceiverAPI.Domain.Entities;
+using ExternalWebhookReceiverAPI.Domain.Entities.Enums;
 
 namespace ExternalWebhookReceiverAPI.Application.Services.Hotmart
 {
@@ -21,6 +24,12 @@ namespace ExternalWebhookReceiverAPI.Application.Services.Hotmart
             {
                 throw new UnauthorizedAccessException(MessageHelper.GetExceptionMessage("EXC0001"));
             }
+
+            ExternalWebhookReceiver externalWebhookReceiver = HotmartWebhookMapper.ToExternalWebhookReceiver(
+                dto: payload,
+                company: company,
+                sourceType: ExternalWebhookReceiverSourceType.Hotmart
+            );
 
             return await Task.FromResult(payload);
         }
