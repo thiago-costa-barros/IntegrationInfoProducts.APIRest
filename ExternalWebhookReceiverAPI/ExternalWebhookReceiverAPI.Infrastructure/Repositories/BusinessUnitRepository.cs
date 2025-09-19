9def55a32f9a0a4f6417c99af6d077590148a58b
thiago-costa-barros <thiagoc.barros92@gmail.com>
2025-09-19 09:21:50 -0300

    feat: Repository BusinessUnit

---
+++ ExternalWebhookReceiverAPI/ExternalWebhookReceiverAPI.Infrastructure/Repositories/BusinessUnitRepository.cs
@@ -0,0 +1,21 @@
+﻿using CommonSolution.Entities.CoreSchema;
+using ExternalWebhookReceiverAPI.Application.Interfaces.DAOs;
+using ExternalWebhookReceiverAPI.Application.Interfaces.Repositories;
+
+namespace ExternalWebhookReceiverAPI.Infrastructure.Repositories
+{
+    public class BusinessUnitRepository : IBusinessUnitRepository
+    {
+        private readonly IBusinessUnitDAO _businessUnitDAO;
+        public BusinessUnitRepository(IBusinessUnitDAO businessUnitDAO)
+        {
+            _businessUnitDAO = businessUnitDAO;
+        }
+        public async Task<BusinessUnit?> GetBusinessUnitByTaxNumberAsync(string? taxNumber)
+        {
+            BusinessUnit? businessUnit = await _businessUnitDAO.GetBusinessUnitByTaxNumberAsync(taxNumber);
+
+            return businessUnit;
+        }
+    }
+}
